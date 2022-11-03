b52c14bab56152a97aa71d4a7d2db88e3930dee8
danghuutien <danghuutientk123@gmail.com>
2022-11-03 09:34:32 +0700

    www

--- src/components/Content.js
+++ src/components/Content.js
@@ -16,7 +16,6 @@ export const Content = () => {
             name: name,
             checked: false,
         };
-
         setTodos([newTodo, ...todos]);
         setName("");
     }
@@ -37,43 +36,43 @@ export const Content = () => {
     }
 
     return (
-    <div className="bg-white p-2.5">
-        <div style={{height:'380px'}} className="mx-5">
-            <div className="flex justify-between" style={{borderBottom: "1px solid gray"}} >
-                <input
-                value={name}
-                onChange={(e) => {
-                    setName(e.target.value);
-                }}
-                
-                className="p-2 w-3/4 focus-visible:outline-none "
-                type="text"
-                placeholder="Add tack...."
-                />
-                <i
-                onClick={addTodo}
-                className="cursor-pointer fa-regular fa-pen-to-square p-2 pr-5"
-                ></i>
+        <div className="bg-white p-2.5">
+            <div style={{height:'380px'}} className="mx-5">
+                <div className="flex justify-between" style={{borderBottom: "1px solid gray"}} >
+                    <input
+                    value={name}
+                    onChange={(e) => {
+                        setName(e.target.value);
+                    }}
+                    
+                    className="p-2 w-3/4 focus-visible:outline-none "
+                    type="text"
+                    placeholder="Add tack...."
+                    />
+                    <i
+                    onClick={addTodo}
+                    className="cursor-pointer fa-regular fa-pen-to-square p-2 pr-5"
+                    ></i>
+                </div>
+
+                <ul style={{height: '330px',overflowY:'scroll'}} >
+                    {todos.map((todo) => (
+                        <li style={{borderBottom:'1px solid gray'}} className="flex justify-between mt-5 py-2" key={todo.id}>
+                            <div>
+                                <input onClick={()=>{handleChecked(todo.id)}} type="checkbox" />
+                                <label style={todo.checked ? {textDecoration: 'line-through'}:{}}  className="ml-2.5">{todo.name}</label>
+                            </div>
+                            <i onClick={() => handleDeleteTodo(todo.id)} className="fa-solid fa-trash cursor-pointer mr-5"></i>
+                        </li>
+                    ))}
+                </ul>
             </div>
 
-            <ul style={{height: '330px',overflowY:'scroll'}} >
-                {todos.map((todo) => (
-                    <li style={{borderBottom:'1px solid gray'}} className="flex justify-between mt-5 py-2" key={todo.id}>
-                        <div>
-                            <input onClick={()=>{handleChecked(todo.id)}} type="checkbox" />
-                            <label style={todo.checked ? {textDecoration: 'line-through'}:{}}  className="ml-2.5">{todo.name}</label>
-                        </div>
-                        <i onClick={() => handleDeleteTodo(todo.id)} className="fa-solid fa-trash cursor-pointer mr-5"></i>
-                    </li>
-                ))}
-            </ul>
+            <div className="flex justify-between py-2">
+                <p>{todos.length} tasks</p>
+                <p>{todos.filter((todo) => todo.checked === true).length} complete</p>
+                <p>{todos.filter((todo) => todo.checked === false).length} open</p>
+            </div>
         </div>
-
-        <div className="flex justify-between py-2">
-            <p>{todos.length} tasks</p>
-            <p>{todos.filter((todo) => todo.checked === true).length} complete</p>
-            <p>{todos.filter((todo) => todo.checked === false).length} open</p>
-        </div>
-    </div>
   );
 };
